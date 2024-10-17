feb437da9add49a261c09407545a48d822c61474
Ayush Srivastava <121169392+ayu2606@users.noreply.github.com>
2024-10-17 15:30:48 +0530

    Made it fastest
    ssspeeeeeeeddddddddddddd

--- src/parser.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (a) {
-    const b = new Set();
-    const c = a.match(/(?:class|className)=["']([^"']+)["']/g) || [];
-    for (const d of c) {
-        const e = d.match(/["']([^"']+)["']/)[1].trim().split(/\s+/);
-        for (const f of e) b.add(f);
-    }
-    return Array.from(b);
-};
